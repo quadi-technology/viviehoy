0357806d3bab50c8f1bc3be40f2497d545dc958f
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2012-12-19 11:41:13 -0500

    Use config on Storify class

--- php/Storify.php
+++ php/Storify.php
@@ -5,7 +5,7 @@ require_once 'Config.php';
 class Storify {
 
   public static function getEntries() {
-    $url = "http://190.3.169.20/pepsicolombia/index.php?historia=pepsi-vivehoy";
+    $url = Config::$storifyService;
 
     $response = file_get_contents($url);
     $data = json_decode($response);
