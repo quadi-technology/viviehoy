cc8a71e71666e7e0d80727e966592e34e77fb58d
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2012-12-19 12:11:04 -0500

    Build output

--- getByDate.php
+++ getByDate.php
@@ -2,6 +2,7 @@
 
 require_once 'php/shared.php';
 
+
 $date = isset($_GET['fecha']) ? $_GET['fecha'] : date('d/m/Y');
 $date = explode('/', $date);
 $date = array_reverse($date);
@@ -9,4 +10,15 @@ $date = implode('-', $date);
 $date = mysql_real_escape_string($date);
 
 $entries = ORM::for_table('entries')->where_raw("DATE(posted_at) = '$date'")->find_many();
+
+$return = array(
+  'contenido' => array(),
+);
+
+foreach ($entries as $entry) {
+  $entry = $entry->as_array();
+  $entry['fecha'] = date('d/m/Y', strtotime($entry['posted_at']));
+  $entry['hora'] = date('H:i', strtotime($entry['posted_at']));
+  $return['contenido'][$entry['hora']][] = $entry;
+}
 
