c6bad019cd8bad0fea4569f896adced6d4a4ba95
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2012-12-21 12:13:48 -0500

    Implement internal storify service

--- php/Config.php
+++ php/Config.php
@@ -2,7 +2,7 @@
 
 class Config {
   public static $db;
-  public static $storifyService = 'http://190.3.169.20/pepsicolombia/index.php?historia=pepsi-vivehoy';
+  public static $story = 'pepsi-vivehoy';
 }
 
 Config::$db = new stdClass();
--- php/Storify.php
+++ php/Storify.php
@@ -5,9 +5,18 @@ require_once 'Config.php';
 class Storify {
 
   public static function getEntries() {
-    $url = Config::$storifyService;
+    $story = Config::$story;
+    $url = "https://api.storify.com/v1/stories/vivehoy/{$story}";
 
-    $response = file_get_contents($url);
+    $ch2 = curl_init();
+    curl_setopt($ch2, CURLOPT_URL, $url);
+    curl_setopt($ch2, CURLOPT_HEADER, false);
+    curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch2, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, 0);
+    $response = curl_exec($ch2);
+    curl_close ($ch2);
+
     $data = json_decode($response);
     $elements = $data->content->elements;
 
