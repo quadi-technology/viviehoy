0854a5194ca7e7b01e59a78bf53c7843f6d42fc1
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2012-12-21 11:48:37 -0500

    Add production mysql info

--- php/Config.php
+++ php/Config.php
@@ -12,5 +12,8 @@ if ($_SERVER['HTTP_HOST'] == 'localhost') {
   Config::$db->username = 'root';
   Config::$db->password = 'root';
 } else {
+  Config::$db->dns = 'mysql:host=localhost;dbname=vivehoy';
+  Config::$db->username = 'root';
+  Config::$db->password = 'S4nch0Bbd0';
 
 }
